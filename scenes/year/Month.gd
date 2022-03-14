d6be2666f77f7f7e6de40e0e2638d07cb3fcd14e
Leole11 <55509229+LeoleUCAB@users.noreply.github.com>
2022-03-14 11:27:48 -0400

    Cambios en la Logica del Calculo de Dias
    - Movida la logica para calcular el numero de dias, y el primer dia de la semana del mes, de la escena de Month a la escena de Year
    - Implementada la logica para que se vean todos los meses del año. Por los momentos, solo se tiene el año actual
    - Reemplazado el nodo padre de la escena de Month de ColorRect a Control node, ya que esta mejor diseñado para objetos de UI
    - Cambiado el efecto de borde de los dias, ahora se puede ver en la simulacion

--- scenes/year/Month.gd
+++ scenes/year/Month.gd
@@ -1,45 +1,28 @@
-extends Node2D
-
-var date
-var daysInMonth
+extends Control
 
 export var dayScene: PackedScene
 
-onready var grid = $ColorRect/GridContainer
+var year
+var month
+var weekStart
+var daysInMonth
+const monthArray = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
 
-const monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+onready var grid = $DayGrid
+onready var monthName = $MonthName
 
 func _ready():
-	pass
-	
-func set_date(parentDate):
-	date = parentDate
-	var month = date.month
-	var year = date.year
-	var isLeapYear
-	if (year % 4 == 0):
-		if (year % 100 == 0):
-			if(year % 400 == 0):
-				isLeapYear = true
-			else:
-				isLeapYear = false
-		else:
-			isLeapYear = true
-	else:
-		isLeapYear = false
-	if month == 2:
-		daysInMonth = 28 + isLeapYear
-	else:
-		daysInMonth = 31 - ((month - 1) % 7 % 2) #it just works don't worry about it: http://www.dispersiondesign.com/articles/time/number_of_days_in_a_month
-	var offset = 3
-	for i in range(daysInMonth + offset):
+	for i in range(daysInMonth + weekStart):
 		var newDay = dayScene.instance()
 		grid.add_child(newDay)
-		newDay.setDayNumber(i+1-offset)
-		if(i < offset):
+		newDay.setDayNumber(i+1-weekStart)
+		if(i < weekStart):
 			newDay.modulate = Color(1, 1, 1, 0)
+		monthName.text = monthArray[month-1]
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func init(param: Dictionary): #{"year": int, "month": int, "weekStart": int, "daysInMonth": int}
+	year = param.year
+	month = param.month
+	weekStart = param.weekStart
+	daysInMonth = param.daysInMonth
+	pass
--- scenes/year/Year.gd
+++ scenes/year/Year.gd
@@ -1,14 +1,48 @@
 extends Node2D
 
-var time = OS.get_datetime()
-onready var month = $Month
+onready var grid = $MonthGrid
 
-# Called when the node enters the scene tree for the first time.
+export var monthScene: PackedScene
+
 func _ready():
-	month.set_date(time)
-	print(time)
+	for i in range(12):
+		var newMonth = monthScene.instance()
+		var year = 2022 #this shouldn't be constant
+		var newMonthDate: Dictionary = {
+			"year": year,
+			"month": i + 1,
+			"day": 1,
+			"hour": 12,
+			"minute": 0,
+			"second": 0
+		}
+		var weekStart = OS.get_datetime_from_unix_time(OS.get_unix_time_from_datetime(newMonthDate)).weekday
+		var initDict: Dictionary = {
+			"year": year,
+			"month": i + 1,
+			"weekStart": weekStart,
+			"daysInMonth": getDaysInMonth(i+1, year)
+		}
+		newMonth.init(initDict)
+		grid.add_child(newMonth)
+	pass
+	
+func getDaysInMonth(month, year):
+	var isLeapYear: bool
+	var daysInMonth: int
+	if (year % 4 == 0):
+		if (year % 100 == 0):
+			if(year % 400 == 0):
+				isLeapYear = true
+			else:
+				isLeapYear = false
+		else:
+			isLeapYear = true
+	else:
+		isLeapYear = false
+	if month == 2:
+		daysInMonth = 28 + isLeapYear as int
+	else:
+		daysInMonth = 31 - ((month - 1) % 7 % 2) #it just works don't worry about it: http://www.dispersiondesign.com/articles/time/number_of_days_in_a_month
+	return daysInMonth
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
